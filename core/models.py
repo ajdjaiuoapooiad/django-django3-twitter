753401e5995eb59abcd53a98c6b4bba9a40fa869
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-06 18:20:15 +0900

    index ok

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from userauth.models import User
+
+
+
+class Post(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    text=models.TextField(max_length=300)
+    
+    def __str__(self):
+        return self.text
--- core/views.py
+++ core/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from core.models import Post
+
 
 
 def index(request):
-    return render(request,'core/index.html')
+    posts=Post.objects.all()
+    
+    context={
+        'posts': posts,
+    }
+    return render(request,'core/index.html',context)
